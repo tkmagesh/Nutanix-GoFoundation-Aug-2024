57fcc66663156709c96f3c53813e661ad3433307
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-19 11:43:07 +0530

    variadic functions

---
+++ 04-functions/02-variadic.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum())
+	fmt.Println(sum(10))
+	fmt.Println(sum(10, 20))
+	fmt.Println(sum(10, 20, 30, 40, 50))
+}
+
+func sum(nos ...int) int {
+	var result int
+	/*
+		for i := 0; i < len(nos); i++ {
+			result += nos[i]
+		}
+	*/
+
+	for _, no := range nos {
+		result += no
+	}
+	return result
+}
