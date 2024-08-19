b5b5665f095d97e343479967b17bfbc04d3e9d28
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-19 14:18:39 +0530

    pointers

---
+++ 05-pointers/01-example.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int
+	no = 100
+
+	var noPtr *int
+	noPtr = &no // data -> address
+	fmt.Println("[main] &no = ", noPtr)
+
+	// dereferencing (address -> data)
+	fmt.Println(*noPtr)
+
+	// in other words
+	fmt.Println(no == *(&no))
+
+	fmt.Println("[main] Before incrementing, no = ", no)
+	// increment(no)
+	incrementPtr(&no)
+	fmt.Println("[main] After incrementing, no = ", no)
+
+	n1, n2 := 100, 200
+	fmt.Printf("Before swapping n1 = %d and n2 = %d\n", n1, n2)
+	swap( /* ? */ )
+	fmt.Printf("After swapping n1 = %d and n2 = %d\n", n1, n2)
+}
+
+func increment(x int) {
+	fmt.Println("[increment] &x = ", &x)
+	x++
+}
+
+func incrementPtr(xPtr *int) {
+	fmt.Println("[increment] xPtr = ", xPtr)
+	(*xPtr)++
+}
+
+func swap( /* ? */ ) /* no return results */ {
+	/* ? */
+}
